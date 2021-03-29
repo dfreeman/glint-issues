1d06bf0ef2544bf6ea9eb193572d6e5fd794bd3b
Boris Petrov <boris_petrov@live.com>
2021-03-29 12:57:37 +0300

    Showcase bug with optional helper positional argument

---
+++ app/helpers/test-helper-1.ts
@@ -0,0 +1,20 @@
+import Helper from '@glint/environment-ember-loose/ember-component/helper';
+
+export interface ITestHelper1 {
+  readonly NamedArgs: { readonly [key: string]: any };
+  readonly PositionalArgs: [string | undefined];
+  readonly Return: string;
+}
+
+export default class TestHelper1 extends Helper<ITestHelper1> {
+  public compute([param]: ITestHelper1['PositionalArgs'], named: ITestHelper1['NamedArgs']): ITestHelper1['Return'] {
+    return param ?? named.foo ?? '';
+  }
+}
+
+declare module '@glint/environment-ember-loose/registry' {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  export default interface Registry {
+    'test-helper-1': typeof TestHelper1;
+  }
+}
